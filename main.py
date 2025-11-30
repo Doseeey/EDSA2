0d29caf63b39158b2963cc2ccfcee0faa4da1c50
Jakub Dębosz <56394099+Doseeey@users.noreply.github.com>
2025-11-30 15:52:23 +0100

    ED tests

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from tau_estimates import estimate_tau_autocorrelation
 
 import numpy as np
 import matplotlib.pyplot as plt
-import nolds
+#Spoko libki moga sie przydac
+#import nolds
+#import hundun
 
 
 n = 10000
@@ -16,16 +18,14 @@ rossler_sig = chaotic_signal("data/rossler_classic.csv", display_plot=display_pl
 rossler09_sig = chaotic_signal("data/rossler_fractional_0_9.csv", display_plot=display_plot)
 rossler088_sig = chaotic_signal("data/rossler_fractional_0_88.csv", display_plot=display_plot)
 
-# rvals = nolds.logarithmic_r(1, np.e, 1.1)
-# corr_dim_args = dict(emb_dim=5, lag=10, fit="poly", rvals=rvals)
-# cdx = nolds.corr_dim(lorenz_sig[:, 0], **corr_dim_args)
-# cdy = nolds.corr_dim(lorenz_sig[:, 1], **corr_dim_args)
-# cdz = nolds.corr_dim(lorenz_sig[:, 2], **corr_dim_args)
-
-# print("Expected correlation dimension:  2.05")
-# print("corr_dim(x)                   : ", cdx)
-# print("corr_dim(y)                   : ", cdy)
-# print("corr_dim(z)                   : ", cdz)
+# TODO
+# Tau estimation - autocorrelation + mutual information
+# embedded dimension - po tau, GP do testa
+# rekonstrukcja na podstawie ed i tau
+# hurst
+# lapunov
+# analiza fraktalna
+# bifurkacja
 
 signals = [random_sig, periodic_sig, lorenz_sig, rossler_sig, rossler09_sig, rossler088_sig]
 
